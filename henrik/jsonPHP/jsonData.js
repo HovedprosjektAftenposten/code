ae39838a5bb9b8b2a33e09923be1d6023ebedbfe
Henrik Eidlaug <eidlaug@gmail.com>
2013-02-21 14:54:59 +0100

    Fetch to multidimensional json ferdig

---
+++ henrik/jsonPHP/jsonData.js
@@ -0,0 +1,72 @@
+{
+   "timeline":[
+      {
+         "tl_ID":"1",
+         "tl_name":"Tidslinje 1",
+         "tl_date":"2013-01-16",
+         "tl_desc":"Test av tl_table",
+         "content":[
+            {
+               "content_ID":"1",
+               "content_time":"16:00:00",
+               "content_date":"2013-01-17",
+               "content_title":"Test",
+               "content_content":"Egyptian riot police clashing with Christians in front of the Coptic Orthodox church in Alexandria on Jan. 1, 2011. A car bombing outside the church killed 21 people. Although many still think of Egypts revolution - and the Arab Spring - as a sudden phenomenon that began in Tunisia, several smaller protests over the past decade helped fuel the movement that reached critical mass last January. One such episode was the bombing of a church in Alexandria on Jan. 1 that killed 21 and injured nearly 100 more. In Egypt, many believed the attack was started by the government to incite anger between Muslims and Christians. (An investigation is still under way).",
+               "content_category":"test",
+               "pictures":[
+                  {
+                     "pic_ID":"1",
+                     "pic_path":"http://i.imgur.com/F6RmDFt.jpg",
+                     "pic_desc":"katt",
+                     "pic_link":"http://i.imgur.com/F6RmDFt.jpg"
+                  },
+                  {
+                     "pic_ID":"5",
+                     "pic_path":"http://i.imgur.com/PVj9VHP.png",
+                     "pic_desc":"Lincoln",
+                     "pic_link":"http://i.imgur.com/PVj9VHP.png"
+                  }
+               ]
+            },
+            {
+               "content_ID":"2",
+               "content_time":"18:00:00",
+               "content_date":"2013-02-01",
+               "content_title":"Test2",
+               "content_content":"Lorem ipsum",
+               "content_category":"testKategori2",
+               "pictures":[
+                  {
+                     "pic_ID":"2",
+                     "pic_path":"http://i.imgur.com/0XnkBk1.jpg",
+                     "pic_desc":"hund",
+                     "pic_link":"http://i.imgur.com/0XnkBk1.jpg"
+                  }
+               ]
+            },
+            {
+               "content_ID":"3",
+               "content_time":"17:00:00",
+               "content_date":"2013-04-02",
+               "content_title":"Test3",
+               "content_content":"Content3 her",
+               "content_category":"kat3",
+               "pictures":[
+                  {
+                     "pic_ID":"3",
+                     "pic_path":"http://i.imgur.com/POum7eK.jpg",
+                     "pic_desc":"seamonster",
+                     "pic_link":"http://i.imgur.com/POum7eK.jpg"
+                  },
+                  {
+                     "pic_ID":"4",
+                     "pic_path":"http://i.imgur.com/uCiPcaO.jpg",
+                     "pic_desc":"mocking dog",
+                     "pic_link":"http://i.imgur.com/uCiPcaO.jpg"
+                  }
+               ]
+            }
+         ]
+      }
+   ]
+}
